f996dbec1726a08b6fa0d18e9ab99c47adc6d31a
Rachel Bock <rachel.online@gmail.com>
2022-02-07 20:28:12 -0700

    Annotate variables file

--- variables.js
+++ variables.js
@@ -1,10 +1,17 @@
+// My name is Rachel Bock.
 var myName = "Rachel Bock"
+// My favorite cat was Cinnamon.
 var myPetsName = "Cinnamon"
+// He was a cat.
 var typeOfPet = "Cat"
 
+// My current car is 7 years old.
 var carAge = 7;
+//It was a 2015 Toyota Prius.
 var carModelYear = 2015;
+//We now have 2 pets.
 var numberOfPets = 2;
+
 
 var isAlive = false;
 var isPlayful = true;
